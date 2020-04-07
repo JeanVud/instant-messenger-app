e1588084ce700ededa977a9384b9c7e63545ba8e
Your Name <you@example.com>
2020-04-07 17:56:49 +0700

    first commit

---
+++ ServerTest.java
@@ -0,0 +1,11 @@
+package com.example.learning.InstantMessaging;
+
+import javax.swing.JFrame;
+
+public class ServerTest {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        server.startRunning();
+    }
+}
